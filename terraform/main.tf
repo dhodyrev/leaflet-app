aa28b48f4b3566f9b3bd150ce41baca834376978
dkhodyriev <hodyrev.dmitriy@gmail.com>
2024-09-13 23:14:09 +0300

    Remove Bucket Creation from Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,19 +5,8 @@ provider "google" {
   region  = "europe-north1"
 }
 
-resource "google_storage_bucket" "static_website" {
-  name                        = "leaflet-site"
-  location                    = "EU"
-  force_destroy               = true
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  uniform_bucket_level_access = true
-}
-
 resource "google_storage_bucket_iam_binding" "public_access" {
-  bucket = google_storage_bucket.static_website.name
+  bucket = "leaflet-site"
   role   = "roles/storage.objectViewer"
   members = [
     "allUsers",
